2767f01da3a175d261193a3ef92d3e4379c02b7a
wbolton <william.bolton@canada.ca>
2021-01-21 16:14:42 -0500

    annotation help

--- api/src/main/java/org/example/entities/employees/EmployeeController.java
+++ api/src/main/java/org/example/entities/employees/EmployeeController.java
@@ -2,15 +2,46 @@ package org.example.entities.employees;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+//For personal reference: https://spring.io/guides/tutorials/rest/
 
 @RestController
 public class EmployeeController {
 
+  @Autowired
+  private final EmployeeRepository employeeRepository;
+
   @GetMapping(value = {"${employees.en}", "${employees.fr}"})
-  public List<EmployeeDTO> employees() {
+  public List<EmployeeDTO> findAll() {
     return Arrays.asList(new EmployeeDTO());
   }
+
+  @GetMapping(value = {"{name}"})
+  public EmployeeDTO findByName(EmployeeDTO anEmployee, @PathVariable String name) {
+    return anEmployee;
+  }
+
+  @GetMapping(value = {"{id}"})
+  public EmployeeDTO findById(EmployeeDTO anEmployee) {
+    return anEmployee;
+  }
+
+  /*
+  @PostMapping("newEmployee")
+  public EmployeeDTO findByid(@RequestBody EmployeeDTO anEmployee) {
+    return anEmployee;
+  }
+
+
+  */
+
+
 }
--- api/src/main/java/org/example/entities/employees/IEmployeeRepository.java
+++ api/src/main/java/org/example/entities/employees/IEmployeeRepository.java
@@ -1,7 +1,16 @@
+
 package org.example.entities.employees;
 
+import java.util.List;
+import java.util.Optional;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-
+    List<EmployeeEntity> findAll();
+    Optional<EmployeeEntity> findByName(String name);
+    Optional<EmployeeEntity> findById(Long id);
 }
