12233a8eee8263d1d6eb2549017eb3f3023185df
wbolton <william.bolton@canada.ca>
2021-01-29 14:04:08 -0500

    heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -13,7 +13,7 @@ EOF
 
 # heroku plugins:install java
 heroku plugins:install heroku-cli-deploy
-heroku deploy:jar "api/build/libs/api##1.0.0.null.jar --app tdd-playwright-example"
+heroku deploy:jar "api/build/libs/api##1.0.0.null.jar" --app tdd-playwright-example
 # heroku run bash -a $HEROKU_APP_NAME
 # git remote add heroku git@heroku.com:$HEROKU_APP_NAME.git
 # heroku git:remote -a $HEROKU_APP_NAME
